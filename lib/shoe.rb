86d18f85ddd63973c7bded6d37ffc4b4f2d50daa
Judy McGoldrick <jjeanius@verizon.net>
2018-01-01 16:22:13 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
+class Shoe
+  attr_accessor :new, :properties,:size, :color, :material, :condition
+  #attr_reader :properties,
+
+  def initialize(new, properties)
+    @new = new
+    @properties = properties
+  end
+
+
+
+  def cobble
+    puts "shoe has been repaired"
+    puts "Your shoe is as good as new!"
+  end
+
+end
 # Make your shoe class here!
